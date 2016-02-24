57867fbae35175dda1739577a9a5c0bcbd534d7b
hoatle <hoatle@teracy.com>
2016-02-24 16:33:28 +0700

    build docs for X.X.x branches

--- .travis/setup.sh
+++ .travis/setup.sh
@@ -8,6 +8,6 @@ elif [[ $TRAVIS_BRANCH == 'develop' ]] ; then
     export DEPLOY_HTML_DIR=develop
 elif [[ $TRAVIS_BRANCH =~ ^v[0-9.]+$ ]]; then
     export DEPLOY_HTML_DIR=${TRAVIS_BRANCH:1}
-elif [[ $TRAVIS_BRANCH =~ ^releases\/[0-9.]+$ ]]; then
+elif [[ $TRAVIS_BRANCH =~ ^[0-9.]+\.x$ || $TRAVIS_BRANCH =~ ^releases\/[0-9.]+$ ]]; then
     export DEPLOY_HTML_DIR=$TRAVIS_BRANCH
 fi
