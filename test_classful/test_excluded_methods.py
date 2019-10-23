11d458effe32944213d9075e6c4dedcd83e74696
hoatle <hoatle@teracy.com>
2019-10-23 13:40:38 +0700

    @ #112 | staticmethod test case for excluded_methods

--- test_classful/test_excluded_methods.py
+++ test_classful/test_excluded_methods.py
@@ -18,9 +18,25 @@ class ExcludedMethodsView(FlaskView):
         return "copy form data"
 
 
+class StaticMethodSetup:
+
+    @staticmethod
+    def setup():
+        # do some sort of crazy expensive calculation on demand here
+        pass
+
+
+class StaticMethodView(StaticMethodSetup, FlaskView):
+    excluded_methods = ["setup"]
+
+    def copy_form_data(self):
+        return "copy form data"
+
+
 app = Flask(__name__)
 NormalMethodsView.register(app)
 ExcludedMethodsView.register(app)
+StaticMethodView.register(app)
 
 client = app.test_client()
 
@@ -33,3 +49,10 @@ def test_normal_methods_copy_form_data():
 def test_excluded_methods_copy_form_data():
     resp = client.get("/excluded-methods/copy_form_data/")
     eq_(resp.status_code, 404)
+
+def test_static_method_view():
+    resp = client.get("/static-method/setup/")
+    eq_(resp.status_code, 404)
+
+    resp = client.get("/static-method/copy_form_data/")
+    eq_(b"copy form data", resp.data)
