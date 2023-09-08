0488dcb7f54254f83287697bc49d71b4e59f8bdf
David Lord <davidism@gmail.com>
2023-09-08 07:56:34 -0700

    remove old dev scripts

--- dev-setup/chef/main-cookbooks/flask-classful/recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook:: flask-classful
-# Recipe:: default
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
-
-
-include_recipe 'flask-classful::setup'
-include_recipe 'flask-classful::docs'
